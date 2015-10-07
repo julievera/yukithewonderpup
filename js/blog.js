1424ac393b280cd2103581ca265b928d73bc083c
code_pig <grodin.robby@gmail.com>
2015-10-06 21:41:13 -0400

    blog now displays 2 posts

--- js/blog.js
+++ js/blog.js
@@ -7,20 +7,18 @@ $(document).ready(function() {
 	$('#newerbutton').click(function(){
 		var offset = parseInt($('#post_content').attr('data-postoffset'));
 		if (offset > 0) {
-			$.get('php/blog.php?postindex=' + (offset-1), function(data) {
+			$.get('php/blog.php?postindex=' + String(offset-2), function(data) {
  				$('#post_content').html(data);
- 				$('#post_content').attr('data-postoffset', offset-1);
+ 				$('#post_content').attr('data-postoffset', offset-2);
 			});
 		};
 	});
 
 	$('#olderbutton').click(function(){
 		var offset = parseInt($('#post_content').attr('data-postoffset'));
-		console.log(offset);
-		$.get('php/blog.php?postindex=' + (offset+1), function(data) {
- 			console.log(data);
+		$.get('php/blog.php?postindex=' + String(offset+2), function(data) {
 			$('#post_content').html(data);
- 			$('#post_content').attr('data-postoffset', offset+1);
+ 			$('#post_content').attr('data-postoffset', offset+2);
 		});
 	});
 });
--- php/blog.php
+++ php/blog.php
@@ -1,26 +1,43 @@
 <?php
-	$post_offset = htmlspecialchars($_GET['postoffset']);
+	$post_offset = htmlspecialchars($_GET['postindex']);
 	$api_uri = "http://api.tumblr.com/v2/blog/yukithewonderpup.tumblr.com/";
 	$api_uri .= "posts?api_key=9qEk24ATIQWXXWwuix1JpQ6dQeiJdEfwGloFO4ADgYs9r60tHV";
-	$api_uri .= "&limit=1&offset=";
+	$api_uri .= "&limit=2&offset=";
 
 	$result = json_decode(file_get_contents($api_uri . $post_offset), true);
 	
-	$post = $result['response']['posts'][0];
-	$post_date = $post['date'];
-	$post_title = $post['title'];
-	$post_body = $post['body'];
+	$first_post = $result['response']['posts'][0];
+	$first_post_date = $first_post['date'];
+	$first_post_title = $first_post['title'];
+	$first_post_body = $first_post['body'];
 
-	
-	$blog_template = "<div id='post_date'>
-                        <h5>" . $post_date . "</h5>
-                    </div>
-                    <div id='post_title'>
-                        <h2>" . $post_title . "</h2>
-                    </div>
-                    <div id='post_text'>
-                    " . $post_body . "
-                    </div>";
+	$second_post = $result['response']['posts'][1];
+	$second_post_date = $second_post['date'];
+	$second_post_title = $second_post['title'];
+	$second_post_body = $second_post['body'];
 
+	$blog_template = "
+		<div id='first_post'>
+                    	<div id='post_date'>
+				<h5>" . $first_post_date . "</h5>
+			    </div>
+			    <div id='post_title'>
+				<h2>" . $first_post_title . "</h2>
+			    </div>
+			    <div id='post_text'>
+			    " . $first_post_body . "
+			    </div>
+		</div>
+		<div id='second_post'>
+			    <div id='post_date'>
+				<h5>" . $second_post_date . "</h5>
+			    </div>
+			    <div id='post_title'>
+				<h2>" . $second_post_title . "</h2>
+			    </div>
+			    <div id='post_text'>
+			    " . $second_post_body . "
+			    </div>
+		</div>";
 	print $blog_template;
 ?>
